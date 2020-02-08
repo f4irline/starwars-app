8e04552728912b8f5816dcc2227aaaad08713669
Tommi Lepola <lepola.tommi@gmail.com>
2020-02-08 12:06:18 +0200

    Basic tests for bmiChart component

--- src/app/components/bmi-chart/bmi-chart.component.spec.ts
+++ src/app/components/bmi-chart/bmi-chart.component.spec.ts
@@ -1,26 +1,56 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BmiChartComponent } from './bmi-chart.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { MatDialogModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChartService, SeriesData } from 'src/app/services/chart/chart.service';
 
 describe('BmiChartComponent', () => {
   let component: BmiChartComponent;
   let fixture: ComponentFixture<BmiChartComponent>;
+  let chartService: ChartService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BmiChartComponent ]
+      declarations: [ BmiChartComponent ],
+      imports: [
+        NgxChartsModule,
+        MatDialogModule,
+        BrowserAnimationsModule
+      ],
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BmiChartComponent);
+    chartService = fixture.debugElement.injector.get(ChartService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    pending();
     expect(component).toBeTruthy();
   });
+
+  it('should initially not have data', (done: DoneFn) => {
+    component.data$.subscribe(data => {
+      expect(data.length).toBe(0);
+      done();
+    });
+  });
+
+  it('should have some data', (done: DoneFn) => {
+    spyOn(chartService, 'charactersToBmiData').and.returnValue([
+      {
+        name: 'Normal'
+      }
+    ] as SeriesData[]);
+
+    component.data$.subscribe(data => {
+      expect(data.length).toBeGreaterThan(0);
+      done();
+    });
+  })
 });
