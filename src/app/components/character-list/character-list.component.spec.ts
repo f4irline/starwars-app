a8b713ad490d6479fe5cf35863d27aaab4ea524f
Tommi Lepola <lepola.tommi@gmail.com>
2020-02-08 12:48:49 +0200

    Basic tests for character list component

--- src/app/components/character-list/character-list.component.spec.ts
+++ src/app/components/character-list/character-list.component.spec.ts
@@ -1,26 +1,187 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CharacterListComponent } from './character-list.component';
+import { CharacterListComponent, SortingBy, SortByProperty } from './character-list.component';
+import { MatIconModule, MatDividerModule, MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Character } from 'src/app/models/character';
 
 describe('CharacterListComponent', () => {
-  let component: CharacterListComponent;
-  let fixture: ComponentFixture<CharacterListComponent>;
+    let component: CharacterListComponent;
+    let fixture: ComponentFixture<CharacterListComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CharacterListComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ CharacterListComponent ],
+            imports: [
+                MatIconModule,
+                MatDividerModule,
+                MatDialogModule,
+            ],
+            providers: [
+                { provide: MatDialogRef, useValue: {} },
+                { provide: MAT_DIALOG_DATA, useValue: [] },
+            ]
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CharacterListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(CharacterListComponent);
+        component = fixture.componentInstance;
+        component.characters = [
+            {
+                name: 'Luke',
+                mass: '85',
+                height: '165',
+            } as Character,
+            {
+                name: 'Steve',
+                mass: '65',
+                height: '185',
+            } as Character,
+            {
+                name: 'Lea',
+                mass: '75',
+                height: '175',
+            } as Character
+        ];
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    pending();
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should return height in meters string', () => {
+        const result = component.getHeightInMetersString('185');
+        expect(result).toEqual('1.85m');
+    });
+
+    it('should return unknown as height', () => {
+        const result = component.getHeightInMetersString('n/a');
+        expect(result).toEqual('Unknown');
+    });
+
+    it('should return mass string', () => {
+        const result = component.getMassString('85');
+        expect(result).toEqual('85kg');
+    });
+
+    it('should return unknown as mass', () => {
+        const result = component.getMassString('n/a');
+        expect(result).toEqual('Unknown');
+    });
+
+    it('should sort characters by mass in descending order', () => {
+        const expectedCharacters: Character[] = [
+            {
+                name: 'Luke',
+                mass: '85',
+                height: '165'
+            } as Character,
+            {
+                name: 'Lea',
+                mass: '75',
+                height: '175'
+            } as Character,
+            {
+                name: 'Steve',
+                mass: '65',
+                height: '185',
+            } as Character
+        ];
+
+        const result = component.sortByMass(true);
+        expect(result).toEqual(expectedCharacters);
+    });
+
+    it('should sort characters by mass in ascending order', () => {
+        const expectedCharacters: Character[] = [
+            {
+                name: 'Steve',
+                mass: '65',
+                height: '185'
+            } as Character,
+            {
+                name: 'Lea',
+                mass: '75',
+                height: '175'
+            } as Character,
+            {
+                name: 'Luke',
+                mass: '85',
+                height: '165',
+            } as Character,
+        ];
+
+        const result = component.sortByMass(false);
+        expect(result).toEqual(expectedCharacters);
+    });
+
+    it('should sort characters by height in descending order', () => {
+        const expectedCharacters: Character[] = [
+            {
+                name: 'Steve',
+                mass: '65',
+                height: '185',
+            } as Character,
+            {
+                name: 'Lea',
+                mass: '75',
+                height: '175',
+            } as Character,
+            {
+                name: 'Luke',
+                mass: '85',
+                height: '165',
+            } as Character,
+        ];
+
+        const result = component.sortByHeight(true);
+        expect(result).toEqual(expectedCharacters);
+    });
+
+    it('should initially have sorting by property as none', () => {
+        component.sortBy$.subscribe(sortBy => {
+            expect(sortBy.property).toEqual(SortByProperty.NONE);
+        });
+    });
+
+    it('should have sorting by property as mass in descending order', () => {
+        component['sortByChange'].next({
+            property: SortByProperty.MASS,
+            descending: true,
+        });
+
+        component.sortBy$.subscribe(sortBy => {
+            expect(sortBy.property).toEqual(SortByProperty.MASS);
+            expect(sortBy.descending).toBeTruthy();
+        });
+    });
+
+    it('should return unfold_more icon string', () => {
+        const sortingByValue: SortingBy = {
+            property: SortByProperty.NONE,
+        };
+        const result = component.getIconString(sortingByValue, SortByProperty.MASS);
+        expect(result).toEqual('unfold_more');
+    });
+
+    it('should return arrow_down icon string', () => {
+        const sortingByValue: SortingBy = {
+            property: SortByProperty.HEIGHT,
+            descending: true,
+        };
+
+        const result = component.getIconString(sortingByValue, SortByProperty.HEIGHT);
+        expect(result).toEqual('arrow_down');
+    });
+
+    it('should return arrow_up icon string', () => {
+        const sortingByValue: SortingBy = {
+            property: SortByProperty.HEIGHT,
+            descending: false,
+        };
+
+        const result = component.getIconString(sortingByValue, SortByProperty.HEIGHT);
+        expect(result).toEqual('arrow_up');
+    });
 });
--- src/app/components/character-list/character-list.component.ts
+++ src/app/components/character-list/character-list.component.ts
@@ -5,89 +5,89 @@ import { MathService } from 'src/app/services/math/math.service';
 import { BehaviorSubject, Observable } from 'rxjs';
 
 export enum SortByProperty {
-  MASS = 'mass',
-  HEIGHT = 'height',
-  NONE = 'none',
+    MASS = 'mass',
+    HEIGHT = 'height',
+    NONE = 'none',
 }
 
 export interface SortingBy {
-  property: SortByProperty;
-  descending?: boolean;
+    property: SortByProperty;
+    descending?: boolean;
 }
 
 @Component({
-  selector: 'app-character-list',
-  templateUrl: './character-list.component.html',
-  styleUrls: ['./character-list.component.scss']
+    selector: 'app-character-list',
+    templateUrl: './character-list.component.html',
+    styleUrls: ['./character-list.component.scss']
 })
 export class CharacterListComponent implements OnInit {
-  sortByProperty = SortByProperty;
-  readonly sortBy$: Observable<SortingBy>;
-  private readonly sortByChange: BehaviorSubject<SortingBy>;
-  constructor(
-    public dialogRef: MatDialogRef<CharacterListComponent>,
-    private mathService: MathService,
-    @Inject(MAT_DIALOG_DATA) public characters: Character[]) {
-      this.sortByChange = new BehaviorSubject<SortingBy>({property: SortByProperty.NONE});
-      this.sortBy$ = this.sortByChange.asObservable();
-      this.sortByChange.subscribe((sortBy) => {
-        if (sortBy.property === SortByProperty.NONE) { return; }
-        this.characters = sortBy.property === SortByProperty.MASS
-          ? this.sortByMass(sortBy.descending)
-          : this.sortByHeight(sortBy.descending);
-      });
+    sortByProperty = SortByProperty;
+    readonly sortBy$: Observable<SortingBy>;
+    private readonly sortByChange: BehaviorSubject<SortingBy>;
+    constructor(
+        public dialogRef: MatDialogRef<CharacterListComponent>,
+        private mathService: MathService,
+        @Inject(MAT_DIALOG_DATA) public characters: Character[]) {
+        this.sortByChange = new BehaviorSubject<SortingBy>({property: SortByProperty.NONE});
+        this.sortBy$ = this.sortByChange.asObservable();
+        this.sortByChange.subscribe((sortBy) => {
+            if (sortBy.property === SortByProperty.NONE) { return; }
+            this.characters = sortBy.property === SortByProperty.MASS
+            ? this.sortByMass(sortBy.descending)
+            : this.sortByHeight(sortBy.descending);
+        });
     }
 
-  onNoClick(): void {
-    this.dialogRef.close();
-  }
+    onNoClick(): void {
+        this.dialogRef.close();
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  getHeightInMetersString(height: string): string {
-    const heightInCm = parseFloat(height);
-    return isNaN(heightInCm) ? 'Unknown' : `${this.mathService.convertCmToMeters(heightInCm)}m`;
-  }
+    getHeightInMetersString(height: string): string {
+        const heightInCm = parseFloat(height);
+        return isNaN(heightInCm) ? 'Unknown' : `${this.mathService.convertCmToMeters(heightInCm)}m`;
+    }
 
-  getMassString(mass: string): string {
-    return isNaN(parseFloat(mass)) ? 'Unknown' : `${mass}kg`;
-  }
+    getMassString(mass: string): string {
+        return isNaN(parseFloat(mass)) ? 'Unknown' : `${mass}kg`;
+    }
 
-  sortBy(property: SortByProperty): void {
-    this.sortByChange.next({
-      property,
-      descending: this.sortByChange.getValue().property ? !this.sortByChange.getValue().descending : true,
-    });
-  }
+    sortBy(property: SortByProperty): void {
+        this.sortByChange.next({
+        property,
+        descending: this.sortByChange.getValue().property ? !this.sortByChange.getValue().descending : true,
+        });
+    }
 
-  sortByMass(descending?: boolean): Character[] {
-    return this.characters.sort((a, b) => {
-      const aMass = parseFloat(a.mass);
-      const bMass = parseFloat(b.mass);
-      if (aMass === bMass) { return 0; }
-      if (isNaN(aMass)) { return 1; }
-      if (isNaN(bMass)) { return -1; }
-      return descending ? bMass - aMass : aMass - bMass;
-    });
-  }
+    sortByMass(descending?: boolean): Character[] {
+        return this.characters.sort((a, b) => {
+        const aMass = parseFloat(a.mass);
+        const bMass = parseFloat(b.mass);
+        if (aMass === bMass) { return 0; }
+        if (isNaN(aMass)) { return 1; }
+        if (isNaN(bMass)) { return -1; }
+        return descending ? bMass - aMass : aMass - bMass;
+        });
+    }
 
-  sortByHeight(descending?: boolean): Character[] {
-    return this.characters.sort((a, b) => {
-      const aHeight = parseFloat(a.height);
-      const bHeight = parseFloat(b.height);
-      if (aHeight === bHeight) { return 0; }
-      if (isNaN(aHeight)) { return 1; }
-      if (isNaN(bHeight)) { return -1; }
-      return descending ? bHeight - aHeight : aHeight - bHeight;
-    });
-  }
+    sortByHeight(descending?: boolean): Character[] {
+        return this.characters.sort((a, b) => {
+        const aHeight = parseFloat(a.height);
+        const bHeight = parseFloat(b.height);
+        if (aHeight === bHeight) { return 0; }
+        if (isNaN(aHeight)) { return 1; }
+        if (isNaN(bHeight)) { return -1; }
+        return descending ? bHeight - aHeight : aHeight - bHeight;
+        });
+    }
 
-  getIconString(sortBy: SortingBy, property: SortByProperty): string {
-    return sortBy.property === property
-      ? sortBy.descending
-        ? 'arrow_down'
-        : 'arrow_up'
-      : 'unfold_more';
-  }
+    getIconString(sortBy: SortingBy, property: SortByProperty): string {
+        return sortBy.property === property
+        ? sortBy.descending
+            ? 'arrow_down'
+            : 'arrow_up'
+        : 'unfold_more';
+    }
 }
